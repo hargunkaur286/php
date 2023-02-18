0ccff35db14cfc4b7d65fe77bd1d95e87d5838b2
hargunkaur286 <hargunkaur286@gmail.com>
2023-02-18 14:15:29 +0530

    basics

---
+++ 01_variables.php
@@ -0,0 +1,24 @@
+<?php 
+echo "This is variables' tutorial<br>";
+
+$name="Hargun";
+$income="200";
+
+echo "This person is $name and her income is $income<br>";
+
+echo "$name is a good girl<br>";
+echo "$name is the real gangsta<br>";
+?>
+
+<h2>
+    Rules for creating variables in PHP:
+</h2>
+<ul>
+    <li>The names of the variables should start with a $ sign.</li>
+    <li>Variables cannot start with a number.</li>
+    <li>Must start with a letter or an underscore</li>
+    <li>Can only contain alpha-numeric and underscores.</li>
+    <!-- <?php $name="Hargun";
+    echo "$name"; ?> -->
+    <li>Variables in PHP are case-sensitive. eg. $harry $Harry $hArry are different.</li>
+</ul>
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # php
+PHP is a web side scripting language which helps in rendering the pages. 
+
+-> VARIABLES are containers for storing information.
+   Variables in PHP start with "$" sign.
+   Variables are case-sensitive.
+   It automatically assigns the data-type to the variable. You don't need to tell whether it is a string or integer, etc. 
