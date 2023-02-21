b5a3bc2cbbd8ebc2a4e1e393986c5ca1cdb21e36
hargunkaur286 <hargunkaur286@gmail.com>
2023-02-21 15:54:57 +0530

    multidimensional arrays

---
+++ 10_multidimensionalArrays.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Welcome to multi-dimensional arrays in php <br>";
+
+//creating a two dimensional array:
+$multiDim = array(
+    array(2, 5, 7, 8),
+    array(1, 2, 6, 0),
+    array(4, 5, 0, 1)
+);
+
+// echo var_dump($multiDim);
+
+//printing contents of a two dimensional array:
+// for ($i=0; $i<count($multiDim); $i++){
+//     echo var_dump($multiDim[$i]);
+//     echo "<br>";
+// }
+
+for ($i=0; $i<count($multiDim); $i++){
+    for ($j=0; $j<count($multiDim[$i]); $j++){
+        echo $multiDim[$i][$j];
+        echo " ";
+    }
+    echo "<br>";
+}
+
+?>
